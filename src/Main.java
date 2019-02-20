319bb991be5c48df11f8312642fcb860a56a0101
ivashchenkoo <alexander.ivashchenkoo@gmail.com>
2019-02-20 20:17:23 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        Room[] room = {new Room(01, "15315sadasd", 2, 250, true, "", 0),
+        new Room(02, "1135103", 3, 360, false, "31.01.2016",7)};
 
+        Hotel hotel = new Hotel(room);
+        hotel.printRooms();
     }
 }
